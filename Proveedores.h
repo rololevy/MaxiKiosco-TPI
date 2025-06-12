53a996ae638e2cc76df8e715d174c106991a9646
lauty002 <110830694+lauty002@users.noreply.github.com>
2025-06-11 23:21:41 -0300

    Add files via upload

--- Proveedores.h
+++ Proveedores.h
@@ -5,32 +5,32 @@
 class Proveedores{
 
 private:
-    std::string _idProveedor;
-    std::string _CUIT;
-    std::string _Nombre;
-    std::string _Telefono;
-    std::string _email;
-    std::string _direccion;
+    int _idProveedor;
+    char _CUIT[30];
+    char _Nombre[30];
+    char _Telefono[30];
+    char _email[30];
+    char _direccion[30];
     bool _estado;
 
 
 public:
 
-std::string getidProveedor() const;
-std::string getCUIT() const;
-std::string getNombre() const;
-std::string getTelefono() const;
-std::string getemail() const;
-std::string getDireccion() const;
-void setidProveedor(const std::string& Proveedor);
-void setCUIT(const std::string& CUIT);
-void setNombre(const std::string& Nombre);
-void setTelefono(const std::string& Telefono);
-void setEmail(const std::string& Email);
-void setDireccion(const std::string& Direccion);
+int getidProveedor() const;
+const char* getCUIT() const;
+const char* getNombre() const;
+const char* getTelefono() const;
+const char* getemail() const;
+const char* getDireccion() const;
+void setidProveedor(const int Proveedor);
+void setCUIT(const char* CUIT);
+void setNombre(const char* Nombre);
+void setTelefono(const char* Telefono);
+void setEmail(const char* Email);
+void setDireccion(const char* Direccion);
 void Cargar();
 void Mostrar();
-void Eliminar();
+void setEstado(bool estado);
 void Opcmenu();
 
 };
